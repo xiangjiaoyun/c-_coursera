f6b4b93c2f9dbcaffa25c2ee28398b0ceb50ddf8
xiangjiaoyun <xjyyuanqing@gmail.com>
2015-11-10 22:09:14 +0800

    change notes

--- A+B_problem/src/A+B_problem.cpp
+++ A+B_problem/src/A+B_problem.cpp
@@ -1,9 +1,9 @@
 //============================================================================
-// Name        : Perceptron.cpp
+// Name        : A+B_problem.cpp
 // Author      : mian
 // Version     :
 // Copyright   : Your copyright notice
-// Description : this is a learning experiment for Perceptron for LiHang
+// Description : this is a test for C++ coding platform. add A and B.
 //============================================================================
 #include <iostream>
 using namespace std;
