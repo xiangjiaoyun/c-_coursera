90e0b5022ead2004633ce2f28b5e9c8c547a475d
xiangjiaoyun <xjyyuanqing@gmail.com>
2015-11-10 22:05:58 +0800

    A+B return C

---
+++ A+B_problem/src/A+B_problem.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Perceptron.cpp
+// Author      : mian
+// Version     :
+// Copyright   : Your copyright notice
+// Description : this is a learning experiment for Perceptron for LiHang
+//============================================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    return 0;
+}
